0e487bf0ac6ad2539d06e38b974fd84b29d25d95
Paul K <64181231+BVC414800@users.noreply.github.com>
2023-02-03 19:05:38 -0700

    Create connection.php
    Connecting MySQL to PHPMyAdmin

---
+++ connection.php
@@ -0,0 +1,23 @@
+<?php
+// Database connection credentials
+$hostname = 'localhost';
+// specify host domain or IP, i.e. 'localhost' or '127.0.0.1' or server IP 'xxx.xxxx.xxx.xxx'
+$database = 'api_db';
+// specify database name
+$db_user = 'root';
+// specify username
+$db_pass = '12345';
+// specify password
+
+$connection = mysqli_connect( "$hostname", "$db_user", "$db_pass", "$database" );
+
+// Check connection
+if ( mysqli_connect_errno() ) {
+    echo 'Failed to connect to MySQL: ' . mysqli_connect_error();
+    exit();
+}
+
+print( '<br />Successfully connected to database:<strong> '.$database.'</strong><br />' );
+print( 'Using host:<strong> '.$hostname.'</strong><br />' );
+print( 'As the user:<strong> '.$db_user.'</strong><br />' );
+?>
